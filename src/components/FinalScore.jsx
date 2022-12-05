402e7ce957f0bd1972964c5de846fe1bcee1e0c2
Miran-Amer <thesweatyog123@gmail.com>
2022-12-05 15:27:56 +0000

    Added Total Score

--- src/components/FinalScore.jsx
+++ src/components/FinalScore.jsx
@@ -10,7 +10,7 @@ const FinalScore = ({score, setItr, setMode, questions}) => {
   return (
     <>
         <div className="flex flex-col justify-center items-center h-screen">
-            <h1 className={`${score > questions.length * 0.5 ?  'text-green-400' : 'text-red-400'} font-bold text-3xl`}>Final Score: {score}</h1>
+            <h1 className={`${score > questions.length * 0.5 ?  'text-green-400' : 'text-red-400'} font-bold text-3xl`}>Final Score: {score} / {questions.length}</h1>
             <button onClick={() => refreshPage()} className='mt-4 px-7 py-3 bg-green-400 border-2 border-green-500 mx-2 rounded-md'>Restart</button>
         </div>
     </>
